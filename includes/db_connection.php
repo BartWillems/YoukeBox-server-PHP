25f09b81dbd2ac6eae2a6c1200ed6a038bb1d511
Bart Willems <ps3bartwillems@gmail.com>
2017-01-24 20:19:26 +0100

    Added db connection
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

---
+++ includes/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST', 'localhost');
+define('DB_USER', 'youkebox_user');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'youkebox');
+
+$mysqli = new mysqli(HOST, DB_USER, DB_PASSWORD, DB_NAME);
+?>
