6401c34dcaa0cb3dd1d1e1596e9a4e715999ebbb
Bart Willems <ps3bartwillems@gmail.com>
2017-01-25 15:30:23 +0100

    Added video_id to the getPlaylist
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

--- get/playlist/index.php
+++ get/playlist/index.php
@@ -9,18 +9,19 @@ function getPlaylist(){
         return 'Database Error';
     }
 
-    $stmt = $mysqli->prepare('SELECT title, duration, added_by, timestamp FROM video ORDER BY timestamp ASC');
+    $stmt = $mysqli->prepare('SELECT title, video_id, duration, added_by, timestamp FROM video ORDER BY timestamp ASC');
     if(!$stmt->execute()) {
         http_response_code(500);
         $stmt->close();
         $mysqli->close();
         return 'Database Execution Error';
     }
-    $stmt->bind_result($title, $duration, $username, $timestamp);
+    $stmt->bind_result($title, $video_id, $duration, $username, $timestamp);
     $videos = array();
     while($stmt->fetch()) {
         $o = null;
         $o['title']     = $title;
+        $o['video_id']  = $video_id;
         $o['duration']  = $duration;
         $o['username']  = $username;
         $o['timestamp'] = $timestamp;
